6f0aa4068aa51cb252b2f3dcfa56cbf1809bee67
Stanislav <stanislav.mostovoy@iMac-de-Delonia-2.local>
2015-05-18 15:58:08 +0200

    SplashScreen gap:plugin

--- mapTest.js
+++ mapTest.js
@@ -21,7 +21,7 @@ function createPage() {
       layoutData: {top: 30, left: "20%", right: "20%"},
       text: "Escribir Archivo"
     }).on("select", function() {
-      Directions.navigateTo("51.50722", "-0.12750"); // latitude, longitude
+      directions.navigateTo("51.50722", "-0.12750"); // latitude, longitude
     }).appendTo(scrollView);
  
 
